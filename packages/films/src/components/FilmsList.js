a252066a22ad1762b30c741097c417a96b433ac3
infoxicator <ruben@infoxication.net>
2021-02-17 00:15:46 +0000

    feat(): fetchye

--- packages/films/src/components/FilmsList.js
+++ packages/films/src/components/FilmsList.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import { useFetchye } from 'fetchye';
 
 export default function FilmsList() {
+  const { isLoading, data } = useFetchye('https://swapi.dev/api/films/');
   return (
     <React.Fragment>
       <main>
         <Container maxWidth="md">
-     Films List
+        {!isLoading && (
+          <div>
+        {data?.body?.results?.map((film) => (
+          <p key={film.episode_id}>{film.title}</p>
+        ))}</div>
+      )}
         </Container>
       </main>
       </React.Fragment>
--- packages/people/src/components/People.js
+++ packages/people/src/components/People.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import { useFetchye } from 'fetchye';
 
 export default function People() {
+  const { isLoading, data } = useFetchye('https://swapi.dev/api/people/');
+  const { isLoading: filmsLoading, data: filmsData } = useFetchye('https://swapi.dev/api/films/');
   return (
     <React.Fragment>
       <main>
         <Container maxWidth="md">
-     People List
+        {!isLoading && (
+          <div>
+        {data?.body?.results?.map((people) => (
+          <p key={people.name}>{people.name}</p>
+        ))}</div>
+      )}
+      {!filmsLoading && (
+          <div>
+        {filmsData?.body?.results?.map((film) => (
+          <p key={film.episode_id}>{film.title}</p>
+        ))}</div>
+      )}
         </Container>
       </main>
       </React.Fragment>
